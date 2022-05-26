7cbdd4ab8646b17968324e0482780b0bd61e0d56
ashaupperla-oc <asha.upperla@eagles.oc.edu>
2022-05-26 12:06:13 -0500

    sprint1 final commit

--- app/controllers/album.controller.js
+++ app/controllers/album.controller.js
@@ -108,6 +108,22 @@ exports.delete = (req, res) => {
       });
     });
 };
+// Delete all Albums from the database.
+exports.deleteAll = (req, res) => {
+  Album.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Albums were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all albums."
+      });
+    });
+};
 // Find all published Albums
 exports.findAllPublished = (req, res) => {
   Album.findAll({ where: { published: true } })
